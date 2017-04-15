d82b8a6252646e98bc3f6ac76deddafd003b2292
Kevin Flansburg <kevinflansburg@ipsec-143-215-17-219.vpn.gatech.edu>
2017-04-15 13:23:56 -0400

    Add detail pages

--- app.py
+++ app.py
@@ -3,8 +3,20 @@ from flask import render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
+def index():
     return render_template('index.html')
+
+@app.route("/ingestion")
+def ingestion():
+    return render_template('ingestion.html')
+
+@app.route("/exploration")
+def exploration():
+    return render_template('exploration.html')
+
+@app.route("/learning")
+def learning():
+    return render_template('learning.html')
 
 if __name__ == "__main__":
     app.run()
